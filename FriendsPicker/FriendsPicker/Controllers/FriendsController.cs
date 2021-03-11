fcd658d78fdefc7c7278e834069b9bcf269cab0a
Robin <robinfuchshofer@gmail.com>
2021-03-11 10:42:35 +0100

    added C# part

---
+++ FriendsPicker/FriendsPicker/Controllers/FriendsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FriendsPicker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FriendsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            return Ok(new[] { "Order 1", "Order 2" });
+        }
+    }
+}
